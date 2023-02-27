e992129d8ac49daf0d829fc7c3cfa30d1d664bf7
sauricat <sauricat@users.noreply.github.com>
2023-02-27 16:19:54 +0000

    Deploying to gh-pages from @ one-among-us/web@8eff7ab639d1d2ff5ac3c3e3865273cd9a9492bd 🚀

---
+++ assets/Ref-2c0806ab.js
@@ -0,0 +1,1 @@
+import{c}from"./index-34eb284e.js";function u(t){return c((e,r)=>{e.computed||(e.computed=Object.create(null)),e.computed[r]={cache:!1,get(){return this.$refs[t||r]}}})}export{u as R};
